7c0e363fe8edd204bcefa94a8c162c8bd5b0a76c
mdparvex <mdparvex443@gmail.com>
2022-08-25 23:53:10 +0600

    added some templates

--- social/urls.py
+++ social/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('volunteer', views.volunteer, name="volunteer"),
+    path('base', views.base, name="base"),
+    
+    
     
 ]
--- social/views.py
+++ social/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'social/index.html')
+def volunteer(request):
+    return render(request, 'social/volunteer.html')
+
+def base(request):
+    return render(request, 'social/base.html')
