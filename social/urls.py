e3ad7476ece90f5610e194b401523a4f8619537b
mdparvex <mdparvex443@gmail.com>
2022-08-26 13:02:22 +0600

    templates added

--- social/urls.py
+++ social/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
+    path('volprofile', views.profile, name="profile"),
+    path('donate', views.donate, name="donate"),
     path('volunteer', views.volunteer, name="volunteer"),
     path('base', views.base, name="base"),
+    
+    
     
     
     
--- social/views.py
+++ social/views.py
@@ -3,8 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'social/index.html')
+
+def register(request):
+    return render(request, 'social/register.html')
+
+def login(request):
+    return render(request, 'social/login.html')
+
+def profile(request):
+    return render(request, 'social/profile.html')
+
+def donate(request):
+    return render(request, 'social/donate.html')
+
 def volunteer(request):
     return render(request, 'social/volunteer.html')
 
 def base(request):
     return render(request, 'social/base.html')
+
+
+
+
